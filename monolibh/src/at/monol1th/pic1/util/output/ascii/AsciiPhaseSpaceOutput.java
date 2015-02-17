efa38988ed80729b5d3dcd4418607d41511e4d46
David Müller <monolithu@gmail.com>
2015-02-17 15:37:09 +0100

    Added some options to the ascii phase space output. It can now render the phase space density, the electric field and the charge density.

--- monolibh/src/at/monol1th/pic1/util/output/ascii/AsciiPhaseSpaceOutput.java
+++ monolibh/src/at/monol1th/pic1/util/output/ascii/AsciiPhaseSpaceOutput.java
@@ -8,13 +8,15 @@ import at.monol1th.pic1.core.particles.Particle;
  */
 public class AsciiPhaseSpaceOutput
 {
-	private Simulation s;
-	private int nx;
+    public boolean drawPhaseSpace = true;
+    public boolean drawElectricField = false;
+    public boolean drawChargeDensity = false;
+    private Simulation s;
+    private int nx;
 	private int ny;
 	private char[][] output;
 	private double[][] psDensity;
     private double max;
-	public boolean drawElectricField = false;
 
 	public AsciiPhaseSpaceOutput(Simulation s, int nx, int ny)
 	{
@@ -45,35 +47,35 @@ public class AsciiPhaseSpaceOutput
 		/*
 			Draw phase space distribution.
 		 */
-		for(Particle p : s.particleList.list)
-		{
-			double px = p.x / size * nx;
-			double pv = (p.vx / c + 1.0) * ny / 2.0;
+        if (drawPhaseSpace) {
+            for (Particle p : s.particleList.list) {
+                double px = p.x / size * nx;
+                double pv = (p.vx / c + 1.0) * ny / 2.0;
 
-			psDensity[(int) pv][(int) px] += 1.0;
+                psDensity[(int) pv][(int) px] += 1.0;
 
 
-		}
+            }
 
-        if (max < 1.0) {
-            for(int x = 0; x < nx; x++) {
+            if (max < 1.0) {
+                for (int x = 0; x < nx; x++) {
+                    for (int y = 0; y < ny; y++) {
+                        if (max < psDensity[y][x]) max = psDensity[y][x];
+                    }
+                }
+            }
+
+            //char[] charSet = {0,'.',176,177,178,219};
+            char[] charSet = {0, '.', ',', ':', ';', '%', '#', 176, 176, 176, 176, 177, 177, 177, 177, 178, 178, 178, 178, 219, 219, 219, 219};
+            //char[] charSet = {0,176,176,176,176,177,177,177,177,178,178,178,178,219,219,219,219};
+            //char[] charSet = {0,176,177,178,219};
+            for (int x = 0; x < nx; x++) {
                 for (int y = 0; y < ny; y++) {
-                    if(max < psDensity[y][x]) max = psDensity[y][x];
+                    int ci = (int) Math.min((psDensity[y][x] / max) * (charSet.length - 1) + 0.5, charSet.length - 1);
+                    if (ci > 0) output[y][x] = charSet[ci];
                 }
             }
         }
-
-		//char[] charSet = {0,'.',176,177,178,219};
-		char[] charSet = {0,'.',',',':',';','%','#',176,176,176,176,177,177,177,177,178,178,178,178,219,219,219,219};
-		//char[] charSet = {0,176,176,176,176,177,177,177,177,178,178,178,178,219,219,219,219};
-		//char[] charSet = {0,176,177,178,219};
-		for(int x = 0; x < nx; x++) {
-			for (int y = 0; y < ny; y++) {
-				int ci = (int) Math.min( (psDensity[y][x]/max) * (charSet.length-1) + 0.5, charSet.length-1);
-				if(ci > 0)  output[y][x] = charSet[ci];
-			}
-		}
-
 		/*
 			Draw electrical field, if activated.
 		 */
@@ -98,7 +100,29 @@ public class AsciiPhaseSpaceOutput
 				output[(int) Ex][x] = 'X';
 			}
 		}
-	}
+
+        /*
+            Draw charge density, if activated.
+		 */
+        if (drawChargeDensity) {
+            double maxd = 0.0;
+            for (int x = 0; x < nx; x++) {
+                int ix = (int) (x / ((double) nx) * s.isizex);
+                double d = s.grid.getCell(ix).d;
+                if (Math.abs(d) > maxd) maxd = Math.abs(d);
+            }
+
+            //maxEx = 0.1;
+
+            for (int x = 0; x < nx; x++) {
+                int ix = (int) (x / ((double) nx) * s.isizex);
+                double d = Math.round(0.5 * ((s.grid.getCell(ix).d / maxd) * 0.25 + 1.0) * ny);
+
+
+                output[(int) d][x] = 'O';
+            }
+        }
+    }
 
 	public String getOutputString()
 	{
