35f430a1740f92f4ca9b91238576797e76c9be35
David Müller <monolithu@gmail.com>
2015-02-19 00:56:09 +0100

    Stuffff

--- prototype2/src/Main.java
+++ prototype2/src/Main.java
@@ -3,6 +3,7 @@ import at.monol1th.pic1.core.grid.solver.Poisson1DFieldSolver;
 import at.monol1th.pic1.core.grid.updater.LeapFrogFieldUpdater;
 import at.monol1th.pic1.core.interpolation.CICInterpolator;
 import at.monol1th.pic1.core.interpolation.NGPInterpolator;
+import at.monol1th.pic1.core.observables.CenterOfMass;
 import at.monol1th.pic1.core.observables.TotalCurrent;
 import at.monol1th.pic1.core.particles.Particle;
 import at.monol1th.pic1.core.particles.movement.PeriodicBoundaryConditions;
@@ -24,21 +25,11 @@ public class Main
 	public static void main(String[] args)
     {
 
-		/*
-				Simulation settings
-		 */
-
-        Settings singleStream = new SingleStreamSettings();
-		Settings twoStream = new TwoStreamSettings();
-        Settings singleParticle = new SingleParticleSettings(-100.0);
-		Settings twoParticle = new TwoParticleSettings();
-		Settings fourParticle = new FourParticleSettings();
-
         /*
                 Simulation initialization
          */
 
-		Simulation sim = new Simulation(singleParticle);
+		Simulation sim = new Simulation(new TwoParticleSettings());
 		sim.initialize();
 
 
@@ -46,29 +37,47 @@ public class Main
                 Create ascii display
          */
 
-        int targetFPS = 3;
-        int computationalStepsPerFrame = 1;
+        int targetFPS = 60;
+        int computationalStepsPerFrame = 2000;
         long optimalTime = 1000000000 / targetFPS;
 
         AsciiDisplay display = new AsciiDisplay(128, 40, sim);
         display.drawPhaseSpace = true;
-        display.drawElectricField = false;
+        display.drawElectricField = true;
         display.drawChargeDensity = false;
-        display.drawCurrentDensity = true;
+        display.drawCurrentDensity = false;
 
         TotalCurrent current = new TotalCurrent(sim);
+        CenterOfMass centerOfMass = new CenterOfMass(sim);
 
-		while (true) {
+        double com0 = centerOfMass.computeCenterOfMass();
+        double relativeAccuracy = Math.pow(10, -5);
+
+        boolean running = true;
+
+		while (running) {
 			long startTime = System.nanoTime();
             for (int t = 0; t < computationalStepsPerFrame; t++)
             {
                 sim.update();
+                if(Math.abs((centerOfMass.computeCenterOfMass() - com0)/com0) > relativeAccuracy)
+                {
+                    System.out.println(String.format("Computational steps until failure: ti = %d", sim.computationalSteps));
+                    System.out.println(String.format("Simulation time until failure: t = %f", sim.elapsedTime));
+                    running = false;
+                    break;
+                }
                // System.out.println(current.computeTotalCurrent());
             }
             display.update(true);
-            display.writeStatusLine(String.format("N = %d, pN = %d", sim.settings.gridSize, sim.particleManager.numberOfParticles), 2, 0);
-            display.writeStatusLine(String.format("dx = %.5f, dt = %.5f", sim.settings.gridSpacing, sim.settings.timeStep), 2, 1);
-            display.writeStatusLine(String.format("t = %.1f, ti = %d", sim.elapsedTime, sim.computationalSteps), 50, 0);
+            display.writeStatusLine(String.format("N = %d; pN = %d; L = %.2f;",
+                            sim.settings.gridSize,
+                            sim.particleManager.numberOfParticles,
+                            sim.settings.gridSize * sim.settings.gridSpacing),
+                    2, 0);
+            display.writeStatusLine(String.format("dx = %.5f; dt = %.5f;", sim.settings.gridSpacing, sim.settings.timeStep), 2, 1);
+            display.writeStatusLine(String.format("t = %.1f; ti = %dl", sim.elapsedTime, sim.computationalSteps), 50, 0);
+            display.writeStatusLine(String.format("c = %.5fl", sim.speedOfLight), 50, 1);
 			long diff = optimalTime - System.nanoTime() + startTime;
 
 			try {
